b302c617df7121d3f0d6fa2c3b6d941d7ad93948
Swastik Sharma <swastkk@gmail.com>
2023-08-17 12:09:28 +0530

    basic Wallpaperchanger added!
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use wallpaper;
+
 fn main() {
-    println!("Hello, world!");
+   println!("{:?}", wallpaper::get());
+   wallpaper::set_from_path("/home/swastik/Downloads/test.png").unwrap();
+   wallpaper::set_mode(wallpaper::Mode::Crop).unwrap();
+   println!("{:?}", wallpaper::get());
 }
