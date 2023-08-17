c359b4149b04e842685c7ac331040801a03887d1
Swastik Sharma <swastkk@gmail.com>
2023-08-17 12:07:43 +0530

    basic Rust setup done
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use wallpaper;
-
 fn main() {
-   println!("{:?}", wallpaper::get());
-   wallpaper::set_from_path("/home/swastik/Downloads/test.png").unwrap();
-   wallpaper::set_mode(wallpaper::Mode::Crop).unwrap();
-   println!("{:?}", wallpaper::get());
+    println!("Hello, world!");
 }
