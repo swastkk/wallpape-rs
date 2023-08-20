71d682bce1caad443985570bda8b7684c943b318
Swastik Sharma <swastkk@gmail.com>
2023-08-20 15:21:15 +0530

    Made Refresh btn working for the Wallpapers
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,35 @@ function App() {
     async function fetchImages() {
       const fetchedImages = await invoke("fetch_wallpapers");
       setImages(fetchedImages);
+      setWallpapers(fetchedImages);
     }
     fetchImages();
   }, []);
 
+
   async function setWallpaper(imagePath) {
-    console.log("Button clicked. Image path:", imagePath);
+    console.log("Setting wallpaper:", imagePath);
     setSelectedImage(imagePath);
-    await invoke("setwallpaper", { imageUrl: imagePath }); 
+    await invoke("setwallpaper", { imageUrl: imagePath });
   }
   async function refreshWallpapers() {
     console.log("Refresh btn clicked!");
     const newWallpapers = await invoke("fetch_wallpapers");
+    console.log("New Wallpapers: ", newWallpapers);
     setWallpapers(newWallpapers);
   }
   return (
     <div className="container">
       <h1>wallpape-rs</h1>
       <div className="image-list">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Image ${index}`}
-            onClick={() => setWallpaper(image)}
-            className={selectedImage === image ? "selected" : ""}
-          />
-        ))}
+      {wallpapers.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Image ${index}`}
+          onClick={() => setWallpaper(image)}
+          className={selectedImage === image ? "selected" : ""} />
+      ))}
       </div>
       <button onClick={refreshWallpapers}>Refresh Wallpapers</button>
     </div>
